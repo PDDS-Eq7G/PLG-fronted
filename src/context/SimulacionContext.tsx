6fde9e82b8be5df6c8af77624e45bcdfb37c64d0
U20212419 <a20212419@pucp.edu.pe>
2025-06-26 15:04:07 -0500

    Ajustada velocidad máxima en colapso

--- src/context/SimulacionContext.tsx
+++ src/context/SimulacionContext.tsx
@@ -97,7 +97,7 @@ export const SimulacionProvider: React.FC<{ children: ReactNode, tipoSimulacion:
   const currentFetchControllerRef = useRef<AbortController | null>(null); // Para abortar peticiones
   const executionIdRef = useRef(0); // Para manejar reinicios de simulación
 
-  const minSpeed = 100; // ms (Faster speed)
+  const minSpeed = tipoSimulacion !== "COLAPSO" ? 100 : 500; // ms (Faster speed)
   const maxSpeed = 2000; // ms (Slower speed)
 
   const velocidadRealRef = useRef(velocidadReal);
